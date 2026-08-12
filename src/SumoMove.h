66c95a49443e482bd83c7f8eb28fb338a9a1cd55
theo-al <emailtheoprauerj@gmail.com>
2026-08-12 02:23:09 -0300

    UNTESTED: limpeza: tudo que pude (+)
    - mudanças de formatação
    - p[] -> pins[]
    - VL,VR -> vl,vr
    - speed0,speed1 -> vl,vr
    - motor_move_t -> move_t
    - removidos métodos não utilizados move_t.set() e DRV8833.pin()
    - estilo
        - tipos mais certos sempre que eu pude
        - membros privados com só a inicial capitalizada, agora começam com _
        - membros privados renomeados quando necessário para coisas mais
          sensatas
    - ledcAttach feito com o mesmo valor em todos os pinos
        - eu refaço o ledcAttach do primeiro pra checar HZ e poder usar
          PWM_HZ em tudo
    - todas as funções que claramente eram só uma versão de 2 motores de
      outras agora só chamam o código da versão de 1 motor 2 vezes em vez de
      repetir
        - também feito isso com off() que era só move(0) e write() que é
          writeRaw()+_vels[vel] etc

--- src/SumoMove.h
+++ src/SumoMove.h
@@ -11,18 +11,14 @@
 
 #include <Arduino.h>
 
-typedef struct motor_move_t {
-  int VL=0, VR=0, dt=0;
-  void set(int vl, int vr, int duration) {
-    VL = vl;
-    VR = vr;
-    dt = duration;
-  }
-} motor_move_t;
+typedef struct {
+  int16_t vl=0, vr=0;
+  unsigned long dt=0;
+} move_t;
 
 class DRV8833 {
   private:
-    int      p[4]; //! -> pins/pinos
+    uint8_t  pins[4];
     uint32_t PWM_HZ  = 25000;
     uint8_t  PWM_RES = 10;
     uint16_t PWM_MAX = 1023;
@@ -30,37 +26,34 @@ class DRV8833 {
     // Sound
     uint16_t SOUND_VOL = 15;
 
-    // Moving
-    bool         Moving         = false;
-    uint32_t     Moving_timeout = 0;
-    int          Moving_list    = 0;
-    motor_move_t Move_next;
-    motor_move_t *Move_list;
+    // Movement
+    bool     _is_moving    = false;
+    uint32_t _move_timeout = 0;
+    size_t   _move_count   = 0;
+    move_t   _next_move;
+    move_t  *_move_list; //!!! onde isso se inicializa? onde se destrói?
 
-    int Speed[2] = {0, 0};
+    int16_t _vels[2] = {0, 0};
 
   public:
     // ===========================================================
     // SETUP
     // ===========================================================
 
-    DRV8833(uint8_t _in1, uint8_t _in2, uint8_t _in3, uint8_t _in4) {
-      p[0] = _in1;
-      p[1] = _in2;
-      p[2] = _in3;
-      p[3] = _in4;
+    DRV8833(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4) {
+      pins[0] = in1; pins[1] = in2; pins[2] = in3; pins[3] = in4;
     }
 
     void begin() { begin(PWM_HZ, PWM_RES); }
 
     void begin(uint32_t f_Hz, uint8_t res) {
-      pinMode(p[0], OUTPUT);
-      pinMode(p[1], OUTPUT);
-      pinMode(p[2], OUTPUT);
-      pinMode(p[3], OUTPUT);
+      pinMode(pins[0], OUTPUT);
+      pinMode(pins[1], OUTPUT);
+      pinMode(pins[2], OUTPUT);
+      pinMode(pins[3], OUTPUT);
 
       Serial.println("[x] DRV8833 Begin");
-      Serial.printf(" | - Pins: [ %d, %d, %d, %d ]\n", p[0], p[1], p[2], p[3]);
+      Serial.printf(" | - Pins: [ %d, %d, %d, %d ]\n", pins[0], pins[1], pins[2], pins[3]);
       Serial.printf(" | - PWM: [ Range: 0 to %d ] [ %d bits / %d Hz ]\n",
                     PWM_MAX, PWM_RES, PWM_HZ);
 
@@ -72,17 +65,19 @@ class DRV8833 {
       #ifdef ESP32
         if ((RES > 0) && (RES <= 12)) PWM_RES = RES;
         PWM_MAX = (1 << PWM_RES) - 1;
-        bool ok0 = ledcAttach(p[0], HZ, PWM_RES);
-        PWM_HZ = ok0 ? HZ : PWM_HZ; //! esquisito, usar o mesmo valor pra todos
-        bool ok1 = ledcAttach(p[1], PWM_HZ, PWM_RES);
-        bool ok2 = ledcAttach(p[2], PWM_HZ, PWM_RES);
-        bool ok3 = ledcAttach(p[3], PWM_HZ, PWM_RES);
+        bool ok = ledcAttach(pins[0], HZ, PWM_RES);
+        PWM_HZ = ok ? HZ : PWM_HZ;
+
+        bool ok0 = ledcAttach(pins[0], PWM_HZ, PWM_RES);
+        bool ok1 = ledcAttach(pins[1], PWM_HZ, PWM_RES);
+        bool ok2 = ledcAttach(pins[2], PWM_HZ, PWM_RES);
+        bool ok3 = ledcAttach(pins[3], PWM_HZ, PWM_RES);
 
         Serial.printf(" | - ledcAttach: [pino %d: %s] [pino %d: %s] [pino %d: %s] [pino %d: %s]\n",
-                      p[0], ok0 ? "OK" : "FALHOU",
-                      p[1], ok1 ? "OK" : "FALHOU",
-                      p[2], ok2 ? "OK" : "FALHOU",
-                      p[3], ok3 ? "OK" : "FALHOU");
+                      pins[0], ok0 ? "OK" : "FALHOU",
+                      pins[1], ok1 ? "OK" : "FALHOU",
+                      pins[2], ok2 ? "OK" : "FALHOU",
+                      pins[3], ok3 ? "OK" : "FALHOU");
         //! retornar erro e mover print pro begin
       #else
         if (RES >= 4 && RES <= 16) PWM_RES = RES;
@@ -100,23 +95,22 @@ class DRV8833 {
       return (true | (PWM_HZ == HZ) << 1 | (PWM_RES == RES) << 2);
     }
 
-    int     pin(uint8_t n) { return (n > 3 ? 0 : p[n]); }
-    int     range()        { return PWM_MAX; }
-    int     read(uint8_t n){ return (n > 1 ? 0 : Speed[n]); }
+    int16_t read(uint8_t n) { return (n > 1 ? 0 : _vels[n]); }
+    int16_t range()         { return PWM_MAX; }
 
     // ===========================================================
     //  ALTO NÍVEL — ACIONAMENTO DIFERENCIAL
     // ===========================================================
 
-    // Controle diferencial clássico
-    // linear/angular em unidades PWM, ou em µs de rádio (radio_range=true)
-    void diff_drive(int linear, int angular, bool invert, bool radio_range) {
+    // Controle diferencial clássico (linear/angular)
+    // entrada em unidades PWM, ou em µs de rádio (com radio_range=true)
+    void diff_drive(int16_t linear, int16_t angular, bool invert, bool radio_range) {
       if (radio_range) {
         linear  = map(linear,  1000, 2000, -PWM_MAX, PWM_MAX);
         angular = map(angular, 1000, 2000, -PWM_MAX, PWM_MAX);
       }
-      int vel_l = constrain(linear + angular, -PWM_MAX, PWM_MAX);
-      int vel_r = constrain(linear - angular, -PWM_MAX, PWM_MAX);
+      int16_t vel_l = constrain(linear + angular, -PWM_MAX, PWM_MAX);
+      int16_t vel_r = constrain(linear - angular, -PWM_MAX, PWM_MAX);
 
       if (invert) move(-vel_r, -vel_l);
       else        move( vel_l,  vel_r);
@@ -127,164 +121,134 @@ class DRV8833 {
     //  ATENÇÃO: chame update() no loop() para que funcionem!
     // ===========================================================
 
-    void clear_moving() { Moving = false; Moving_list = 0; }
+    void clear_moving() { _is_moving = false; _move_count = 0; }
 
     // Move indefinidamente
-    void move(motor_move_t m) {
-      move(m.VL, m.VR);
-      Moving_timeout = millis() + m.dt;
-      Moving = true;
+    void move(move_t m) {
+      move(m.vl, m.vr);
+      _move_timeout = millis() + m.dt;
+      _is_moving = true;
     }
 
     // Executa uma lista de movimentos em sequência
-    void move(motor_move_t *m, int len) {
+    void move(move_t *m, size_t len) {
       move(*m);
-      Move_list   = m + 1;
-      Moving_list = len - 1;
+      _move_list  = m + 1;
+      _move_count = len - 1;
     }
 
     // Move por 'duration' ms e para em seguida
-    void move_for(int speed_0, int speed_1, uint32_t duration) {
-      Moving         = true;
-      Moving_timeout = millis() + duration;
-      move(speed_0, speed_1);
-      Moving_list = 0;
+    void move_for(int16_t vl, int16_t vr, uint32_t duration) {
+      move(vl, vr);
+      _is_moving = true;
+      _move_count = 0;
+      _move_timeout = millis() + duration;
     }
 
     // Move por 'duration' ms e depois executa um segundo movimento
-    void move_for_then(int speed_0, int speed_1, uint32_t duration,
-                       int speed_next_0, int speed_next_1, uint32_t duration_next) {
-      Moving         = true;
-      Moving_timeout = millis() + duration;
-      move(speed_0, speed_1);
+    void move_for_then(int16_t vl,      int16_t vr,      uint32_t duration,
+                       int16_t vl_next, int16_t vr_next, uint32_t duration_next) {
+      _is_moving = true;
+      _move_timeout = millis() + duration;
+      move(vl, vr);
 
-      Moving_list  = 1;
-      Move_next.VL = speed_next_0;
-      Move_next.VR = speed_next_1;
-      Move_next.dt = duration_next;
-      Move_list    = &Move_next;
+      //!!! isso vaza uma referência a um ponteiro de pilha
+      _move_count = 1;
+      _next_move.vl = vl;
+      _next_move.vr = vr;
+      _next_move.dt = duration_next;
+      _move_list    = &_next_move;
     }
 
     // Deve ser chamado no loop() para processar os timers
     int update() {
-      if (!Moving) return -1;
+      if (!_is_moving) return -1;
 
-      if (millis() >= Moving_timeout) {
-        Moving = false;
-        if (Moving_list) {
-          Moving_list--;
-          Moving          = true;
-          Moving_timeout += Move_list->dt;
+      if (millis() >= _move_timeout) {
+        _is_moving = false;
+        if (_move_count) {
+          _is_moving = true;
+          _move_count--;
+          _move_timeout += _move_list->dt;
 
-          if (abs(Move_list->VL) <= PWM_MAX) write(Move_list->VL, 0); else stop(0);
-          if (abs(Move_list->VR) <= PWM_MAX) write(Move_list->VR, 1); else stop(1);
-          Move_list++;
+          if (abs(_move_list->vl) <= PWM_MAX) write(_move_list->vl, 0); else stop(0);
+          if (abs(_move_list->vr) <= PWM_MAX) write(_move_list->vr, 1); else stop(1);
+          _move_list++;
         }
       }
 
-      return (Moving + Moving_list);
+      return (_is_moving + _move_count);
     }
 
     // ===========================================================
-    //  MOVIMENTOS DIRETOS
+    //  BAIXO NÍVEL — MOVIMENTOS DIRETOS E ESCRITA PWM
     // ===========================================================
 
-    void move   (int s0, int s1) { write   (s0, 0); write   (s1, 1); }
-    void moveSD (int s0, int s1) { writeSD (s0, 0); writeSD (s1, 1); }
-    void moveRaw(int s0, int s1) { writeRaw(s0, 0); writeRaw(s1, 1); }
+    void move   (int16_t vl, int16_t vr) { write   (vl, 0); write   (vr, 1); }
+    void moveSD (int16_t vl, int16_t vr) { writeSD (vl, 0); writeSD (vr, 1); }
+    void moveRaw(int16_t vl, int16_t vr) { writeRaw(vl, 0); writeRaw(vr, 1); }
 
     // Freio ativo (curto-circuita os terminais — para mais rápido)
-    void stop() {
-      #ifdef ESP32
-        ledcWrite(p[0], PWM_MAX); ledcWrite(p[1], PWM_MAX);
-        ledcWrite(p[2], PWM_MAX); ledcWrite(p[3], PWM_MAX);
-      #else
-        analogWrite(p[0], PWM_MAX); analogWrite(p[1], PWM_MAX);
-        analogWrite(p[2], PWM_MAX); analogWrite(p[3], PWM_MAX);
-      #endif
-      Speed[0] = 0; Speed[1] = 0;
-    }
+    void stop() { stop(0); stop(1); }
 
     void stop(uint8_t motor) {
       if (motor > 1) return;
       #ifdef ESP32
-        ledcWrite(p[0 + 2*motor], PWM_MAX);
-        ledcWrite(p[1 + 2*motor], PWM_MAX);
+        ledcWrite(pins[0 + 2*motor], PWM_MAX);
+        ledcWrite(pins[1 + 2*motor], PWM_MAX);
       #else
-        analogWrite(p[0 + 2*motor], PWM_MAX);
-        analogWrite(p[1 + 2*motor], PWM_MAX);
+        analogWrite(pins[0 + 2*motor], PWM_MAX);
+        analogWrite(pins[1 + 2*motor], PWM_MAX);
       #endif
-      Speed[motor] = 0;
+      _vels[motor] = 0;
     }
 
     // Corte de energia (motor desacelera por inércia)
-    void off() { //! -> off(0); off(1);
-      #ifdef ESP32
-        ledcWrite(p[0], 0); ledcWrite(p[1], 0);
-        ledcWrite(p[2], 0); ledcWrite(p[3], 0);
-      #else
-        analogWrite(p[0], 0); analogWrite(p[1], 0);
-        analogWrite(p[2], 0); analogWrite(p[3], 0);
-      #endif
-      Speed[0] = 0; Speed[1] = 0;
-    }
+    void off()              { off(0); off(1); }
+    void off(uint8_t motor) { write(motor, 0); }
 
-    void off(uint8_t motor) {
+    // Escrita com sinal de velocidade salvo em _vels[]
+    void write(int16_t vel, uint8_t motor) {
       if (motor > 1) return;
-      #ifdef ESP32
-        ledcWrite(p[0 + 2*motor], 0);
-        ledcWrite(p[1 + 2*motor], 0);
-      #else
-        analogWrite(p[0 + 2*motor], 0);
-        analogWrite(p[1 + 2*motor], 0);
-      #endif
-      Speed[motor] = 0;
+      writeRaw(vel, motor);
+      _vels[motor] = vel;
     }
 
-    // ===========================================================
-    //  BAIXO NÍVEL — ESCRITA PWM
-    // ===========================================================
-
-    // Escrita com sinal de velocidade salvo em Speed[]
-    void write(int speed, int motor) {
-      if (motor > 1) return;
-      Speed[motor] = speed;
-      //! writeRaw em vez disso embaixo
-      #ifdef ESP32
-        if (speed >= 0) { ledcWrite(p[0+2*motor],  speed); ledcWrite(p[1+2*motor], 0); }
-        else            { ledcWrite(p[1+2*motor], -speed); ledcWrite(p[0+2*motor], 0); }
-      #else
-        int m = 2 * motor;
-        if (speed >= 0) { analogWrite(p[0+m],  speed); analogWrite(p[1+m], 0); }
-        else            { analogWrite(p[1+m], -speed); analogWrite(p[0+m], 0); }
-      #endif
-    }
-
-    // Escrita raw (sem salvar em Speed[])
-    void writeRaw(int speed, int motor) { //! checar -speed e -(-speed)
+    // Escrita raw (sem salvar em _vels[])
+    void writeRaw(int16_t vel, uint8_t motor) {
       if (motor > 1) return;
       const int m = 2*motor;
       #ifdef ESP32
-        if (speed >= 0) { ledcWrite(p[0+m],  speed); ledcWrite(p[1+m], 0); }
-        else            { ledcWrite(p[1+m], -speed); ledcWrite(p[0+m], 0); }
+        if (vel >= 0) { ledcWrite(pins[0+m],  vel); ledcWrite(pins[1+m], 0); }
+        else          { ledcWrite(pins[1+m], -vel); ledcWrite(pins[0+m], 0); }
       #else
-        if (speed >= 0) { analogWrite(p[0+m],  speed); analogWrite(p[1+m], 0); }
-        else            { analogWrite(p[1+m], -speed); analogWrite(p[0+m], 0); }
+        if (vel >= 0) { analogWrite(pins[0+m],  vel); analogWrite(pins[1+m], 0); }
+        else          { analogWrite(pins[1+m], -vel); analogWrite(pins[0+m], 0); }
       #endif
     }
 
     // Modo slow-decay (frenagem mais suave)
-    void writeSD(int speed, int motor) { //! checar -speed e MAX+speed
+    void writeSD(uint16_t vel, int8_t motor) { //! checar se isso funciona e como
       if (motor > 1) return;
-      Speed[motor] = speed; //! era pra ser isso mesmo ou o que usa embaixo?
+      _vels[motor] = vel; //! era pra ser isso mesmo ou o que usa embaixo?
       const int m = 2*motor;
-      //! writeRaw(MAX+-speed) em vez disso embaixo
+      //! writeRaw(MAX+-vel) em vez disso embaixo
       #ifdef ESP32
-        if (speed < 0) { ledcWrite(p[0+m], PWM_MAX+speed); ledcWrite(p[1+m], PWM_MAX); }
-        else           { ledcWrite(p[1+m], PWM_MAX-speed); ledcWrite(p[0+m], PWM_MAX); }
+        if (vel >= 0) {
+            ledcWrite(pins[0+m], PWM_MAX);
+            ledcWrite(pins[1+m], PWM_MAX-vel);
+        } else {
+            ledcWrite(pins[0+m], PWM_MAX+vel); // já negativo
+            ledcWrite(pins[1+m], PWM_MAX);
+        }
       #else
-        if (speed < 0) { analogWrite(p[0+m], PWM_MAX+speed); analogWrite(p[1+m], PWM_MAX); }
-        else           { analogWrite(p[1+m], PWM_MAX-speed); analogWrite(p[0+m], PWM_MAX); }
+        if (vel < 0) {
+            analogWrite(pins[0+m], PWM_MAX+vel);
+            analogWrite(pins[1+m], PWM_MAX);
+        } else {
+            analogWrite(pins[0+m], PWM_MAX);
+            analogWrite(pins[1+m], PWM_MAX-vel); // já negativo
+        }
       #endif
     }
 
@@ -296,7 +260,8 @@ class DRV8833 {
     uint16_t sound_duty()           { return SOUND_VOL * (PWM_MAX / 100); }
 
     // Emite n bipes em ambos os motores
-    void bip(uint8_t n, uint16_t dt, uint32_t tone) {
+    void bip(uint8_t n, uint16_t dt, uint32_t tone) { //! unificar com bip(motor)
+                                                      //! bip->beep/bipe
       sound_tone(tone);
       uint16_t duty = sound_duty();
       for (int i = 0; i < n; i++) {
@@ -323,25 +288,25 @@ class DRV8833 {
     void sound_tone(uint32_t tone, uint8_t motor) {
       if (motor > 1) return;
       #ifdef ESP32
-        ledcAttach(p[0 + 2*motor], tone, PWM_RES);
-        ledcAttach(p[1 + 2*motor], tone, PWM_RES);
+        ledcAttach(pins[0 + 2*motor], tone, PWM_RES);
+        ledcAttach(pins[1 + 2*motor], tone, PWM_RES);
       #else
         analogWriteFreq(tone);
       #endif
       writeRaw(sound_duty(), motor);
     }
 
-    void sound_stop()              { sound_stop(0); sound_stop(1); }
+    void sound_stop() { sound_stop(0); sound_stop(1); }
 
     void sound_stop(uint8_t motor) {
       if (motor > 1) return;
       #ifdef ESP32
-        ledcAttach(p[0 + 2*motor], PWM_HZ, PWM_RES);
-        ledcAttach(p[1 + 2*motor], PWM_HZ, PWM_RES);
+        ledcAttach(pins[0 + 2*motor], PWM_HZ, PWM_RES);
+        ledcAttach(pins[1 + 2*motor], PWM_HZ, PWM_RES);
       #else
         analogWriteFreq(PWM_HZ);
       #endif
-      write(Speed[motor], motor);
+      write(_vels[motor], motor);
     }
 };
 
